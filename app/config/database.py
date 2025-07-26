c604816a5f5414120f74dbd93c20b20979b50663
Jose David Castillo <jose1xdd@gmail.com>
2025-07-26 09:29:51 -0500

    conexion a base de datos

--- app/config/database.py
+++ app/config/database.py
@@ -4,7 +4,11 @@ from app.utils.enviroment import settings
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Configuración síncrona
-engine = create_engine(settings.database_url)
+engine = create_engine(settings.database_url,
+                       pool_pre_ping=True,     # ✔️ Verifica que la conexión está viva antes de usarla
+                       pool_recycle=280,       # ✔️ Cierra y recrea conexiones inactivas después de N segundos
+                       pool_size=10,           # Tamaño del pool
+                       max_overflow=20,)
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
