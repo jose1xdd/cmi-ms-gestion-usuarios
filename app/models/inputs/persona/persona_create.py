194e6d96736f9b6683403ab6bf5da77cda0695d6
Jose David Castillo <jose1xdd@gmail.com>
2025-11-26 08:50:08 -0500

    ajuste creacion

--- app/models/inputs/persona/persona_create.py
+++ app/models/inputs/persona/persona_create.py
@@ -8,20 +8,24 @@ class PersonaCreate(BaseModel):
     """
     Modelo de entrada para crear una Persona.
     """
-    id: str = Field(max_length=36, description="Documento o identificador único")
+    id: str = Field(
+        max_length=36, description="Documento o identificador único")
     tipoDocumento: EnumDocumento
     nombre: str = Field(max_length=50)
     apellido: str = Field(max_length=50)
     fechaNacimiento: date
-    parentesco: EnumParentesco
+    parentesco: Optional[EnumParentesco] = None
     sexo: EnumSexo
     profesion: Optional[str] = Field(default=None, max_length=100)
     escolaridad: EnumEscolaridad
     direccion: str = Field(max_length=200)
     telefono: str = Field(max_length=20)
-    idFamilia: Optional[int] = Field(default=None, description="ID de la familia a la que pertenece")
-    idParcialidad: int = Field(default=None, description="ID de la parcialidad")
-    fechaDefuncion: Optional[date] = Field(default=None, description="Fecha de defunción si la persona ha fallecido")
+    idFamilia: Optional[int] = Field(
+        default=None, description="ID de la familia a la que pertenece")
+    idParcialidad: int = Field(
+        default=None, description="ID de la parcialidad")
+    fechaDefuncion: Optional[date] = Field(
+        default=None, description="Fecha de defunción si la persona ha fallecido")
 
     class Config:
         from_attributes = True
